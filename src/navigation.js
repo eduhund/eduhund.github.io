31141e90d5839e02be31b17779363f18146a2cc0
Olga Buberman <bub@sobakapav.ru>
2024-04-24 14:28:38 +0300

    Images, Links

--- src/navigation.js
+++ src/navigation.js
@@ -88,11 +88,11 @@ export const footerData = {
   ],
   socialLinks: [
   //  { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-  { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/neverhund' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/neverhund' },
   //  { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
   //  { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-  { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/eduhund' }
-  //],
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/eduhund' },
+  ],
   //footNote: `
   //  <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
   //  Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
